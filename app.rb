b4493280ce2ae8f2e7a13b28334a0c3917f21888
thekman <thekman@cj.com>
2016-05-01 14:35:24 -0700

    guid array

--- app.rb
+++ app.rb
@@ -11,14 +11,31 @@ end
 post '/music' do
   text = params[:text]
   puts '### text is: ' + text
+
+  guidArray = Array.new
+
   links = URI.extract(text)
   links.each do |link|
     puts '### link argument is ' + link
     if link =~ /\A#{URI::regexp}\z/ && valid_websites(link)
       guid = link[/https:\/\/www.youtube.com\/watch\?v=(.*)/, 1]
+      guidArray.push guid
       puts '### guid is ' + guid
     end
   end
+
+  print_guids(guidArray)
+
+end
+
+def print_guids(guidArray)
+  puts '### All valid youtube links:'
+  puts ''
+  guidArray.each do |guid|
+    puts guid
+  end
+  puts ''
+  puts '###'
 end
 
 def valid_websites(url)
