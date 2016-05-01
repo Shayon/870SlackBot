8d03215d12e20185f47e700967d0fc973e4f0ad5
Shayon <shyjavadi@gmail.com>
2016-05-01 14:09:00 -0700

    Add support for multiple links and other text

--- app.rb
+++ app.rb
@@ -9,25 +9,24 @@ get '/' do
   '#{$foo}'
 end
 
-post '/music/:isValid/' do
-	puts 'successfully hit post'
-  if params[:isValid] == 'true'
-    link = 'https://www.youtube.com/watch?v=P-gGZ2rJqfQ'
-  else
-    link = 'youtube'
-  end
-  puts 'link arguement is ' + link
-	if link =~ /\A#{URI::regexp}\z/ && valid_websites(link)
-    gid = link[/https:\/\/www.youtube.com\/watch\?v=(.*)/, 1]
-    puts 'gid is ' + gid
-    puts 'uri thing works'
-  else
-    puts 'uri thing does not work'
-  end
-  if valid_websites(link)
-		puts 'arguement contains youtube'
-  else
-    puts 'arguement is invalid'
+post '/music' do
+  text = params[:text]
+  puts "### text is: " + text
+  links = URI.extract(text)
+  links.each do |link|
+    puts '### link arguement is ' + link
+    if link =~ /\A#{URI::regexp}\z/ && valid_websites(link)
+      guid = link[/https:\/\/www.youtube.com\/watch\?v=(.*)/, 1]
+      puts '### guid is ' + guid
+      puts '### uri thing works'
+    else
+      puts '### uri thing does not work'
+    end
+    if valid_websites(link)
+      puts '### arguement contains youtube'
+    else
+      puts '### arguement is invalid'
+    end
   end
 end
 
