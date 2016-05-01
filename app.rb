dd43a903768ccf0242703019c85893e5e1df45a4
thekman <thekman@cj.com>
2016-05-01 13:03:34 -0700

    uri detection and parsing

--- app.rb
+++ app.rb
@@ -5,15 +5,30 @@ require 'uri'
 $foo = 'hello'
 
 get '/' do
+  puts 'hi'
   '#{$foo}'
 end
 
-post '/music/' do
-	puts 'success'
-	#text = params[:text]
-	#if text =~ /\A#{URI::regexp}\z/ && valid_websites(text)
-	#	puts "okay"
-	#end
+post '/music/:isValid/' do
+	puts 'successfully hit post'
+  if params[:isValid] == 'true'
+    link = 'https://www.youtube.com/watch?v=P-gGZ2rJqfQ'
+  else
+    link = 'youtube'
+  end
+  puts 'link arguement is ' + link
+	if link =~ /\A#{URI::regexp}\z/ && valid_websites(link)
+    gid = link[/https:\/\/www.youtube.com\/watch\?v=(.*)/, 1]
+    puts 'gid is ' + gid
+    puts 'uri thing works'
+  else
+    puts 'uri thing does not work'
+  end
+  if valid_websites(link)
+		puts 'arguement contains youtube'
+  else
+    puts 'arguement is invalid'
+  end
 end
 
 def valid_websites(url)
