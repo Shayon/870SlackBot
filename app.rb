4516cab30886049b8c769b273463e4aed9f12a0c
thekman <thekman@cj.com>
2016-05-01 14:23:02 -0700

    cleanup

--- app.rb
+++ app.rb
@@ -1,31 +1,22 @@
 require 'sinatra'
-require 'json'
 require 'uri'
 
 $foo = 'hello'
 
 get '/' do
   puts 'hi'
-  '#{$foo}'
+  'get successful'
 end
 
 post '/music' do
   text = params[:text]
-  puts "### text is: " + text
+  puts '### text is: ' + text
   links = URI.extract(text)
   links.each do |link|
-    puts '### link arguement is ' + link
+    puts '### link argument is ' + link
     if link =~ /\A#{URI::regexp}\z/ && valid_websites(link)
       guid = link[/https:\/\/www.youtube.com\/watch\?v=(.*)/, 1]
       puts '### guid is ' + guid
-      puts '### uri thing works'
-    else
-      puts '### uri thing does not work'
-    end
-    if valid_websites(link)
-      puts '### arguement contains youtube'
-    else
-      puts '### arguement is invalid'
     end
   end
 end
